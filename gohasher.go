d88ea67c490338944946b0721bbf3c4b5d824830
FonderElite <s1170044@usls.edu.ph>
2021-10-24 09:09:44 +0000

    Update gohasher.go

--- gohasher.go
+++ gohasher.go
@@ -2,6 +2,7 @@ package main
 import (
 "crypto/md5"
 "golang.org/x/crypto/md4"
+"golang.org/x/crypto/ripemd160"
 "crypto/sha1"
 "crypto/sha256"
 "fmt"
@@ -24,7 +25,7 @@ banner := `
 cyan := color.New(color.FgCyan).Add(color.Underline)
 green := color.New(color.FgGreen).Add(color.Underline)
 red := color.New(color.FgRed).Add(color.Underline)
-figure.NewFigure("Go-Hasher", "basic", true).Scroll(1000, 200, "left")
+figure.NewFigure("Go-Hasher", "basic", true).Scroll(2000, 200, "left")
 fmt.Println(banner)
 fmt.Println("\nMade By Gian Paris C. Agsam | Github:https://github.com/FonderElite\n")
 cyan.Println("Build: sudo go build <file_name.go>")
@@ -34,9 +35,9 @@ red.Println("Help: sudo go run <file_name.go> -h")
 func hashstring(strtohash string, typeofhash string) *Hasher{
 hasher := Hasher{str_tohash:strtohash, hashtype:typeofhash}
 if strings.ToLower(hasher.hashtype) == "md4"{
-md4hasher := md4.New()
-md4hasher.Write([]byte(hasher.str_tohash))
-md4_hashedstr :=  hex.EncodeToString(md4hasher.Sum(nil))
+        md4hasher := md4.New()
+        md4hasher.Write([]byte(hasher.str_tohash))
+        md4_hashedstr :=  hex.EncodeToString(md4hasher.Sum(nil))
 fmt.Printf("MD4 - %v : %v",hasher.str_tohash, md4_hashedstr)
 }else if strings.ToLower(hasher.hashtype) == "md5"{
 md5hasher := md5.New()
@@ -53,6 +54,11 @@ sha256hasher := sha256.New()
 sha256hasher.Write([]byte(hasher.str_tohash))
 sha256_hashedstr := hex.EncodeToString(sha256hasher.Sum(nil))
 fmt.Printf("SHA256 - %v : %v",hasher.str_tohash, sha256_hashedstr)
+}else if strings.ToLower(hasher.hashtype) == "ripemd160"{
+ripemdhasher := ripemd160.New()
+ripemdhasher.Write([]byte(hasher.str_tohash))
+ripemd_hashedstr := hex.EncodeToString(ripemdhasher.Sum(nil))
+fmt.Printf("RIPEMD160 - %v : %v",hasher.str_tohash, ripemd_hashedstr)
 }else{
 error_msg := color.New(color.FgYellow, color.Bold)
 error_msg.Println("\nOptions: ")
