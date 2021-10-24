439d3c61964a73fa3b6ff66c28e1e4d1977c39c1
FonderElite <s1170044@usls.edu.ph>
2021-10-24 03:12:37 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 function check_dependencies(){
-        read -p "Would you like to update?:(n=0/y=1): " choice
-        if [[ $choice == "0" ]];then
-                echo "Skipping Update..."
-                sleep 1 
-        elif [[ $choice == "1" ]];then
-                sudo apt update
-        fi
+	read -p "Would you like to update?:(n=0/y=1): " choice
+	if [[ $choice == "0" ]];then
+		echo "Skipping Update..."
+		sleep 1 
+	elif [[ $choice == "1" ]];then
+		sudo apt update
+	fi
 if [ ! go ];
 then
-        sudo apt install golang
+	sudo apt install golang
 else
-        echo "Go lang is present in: $(which go)"
-        sleep 1
-        echo "Skipping installation..."
-        sleep 1
+	echo "Go lang is present in: $(which go)"
+	sleep 1
+	echo "Skipping installation..."
+	sleep 1
 fi
 FILE=go.mod 
 if test -f "$FILE";
 then
-        echo "$FILE is present in $(pwd)..."
-        sleep 1
+	echo "$FILE is present in $(pwd)..."
+	sleep 1
 
 else
 
-        read -p "Project name for go mod" projname
-        sudo go mod init $projname
+	read -p "Project name for go mod" projname
+	sudo go mod init $projname
 fi
 }
 
